552f5c55c720676d395267af301662215a5f87fe
Jim Ladd <jladd@tuta.io>
2025-12-07 20:29:47 -0800

    toil: add test for get_random_bytes

--- pad/tests/test_hazmat.py
+++ pad/tests/test_hazmat.py
@@ -1,7 +1,14 @@
 from unittest import mock
 
-from perfect_noise.hazmat import hazmat
+from perfect_pad.hazmat.hazmat import get_random_bytes
 
 
-def test_foo():
-    pass
+@mock.patch("perfect_pad.hazmat.hazmat.urandom")
+def test_get_random_bytes(mock_urandom):
+    mock_result = b"\x13\xce\xfa\x95"
+    mock_urandom.return_value = mock_result
+
+    res = get_random_bytes(5)
+
+    mock_urandom.assert_called_once_with(5)
+    assert res == mock_result
