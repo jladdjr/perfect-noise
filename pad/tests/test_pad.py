1dfd25de6858e2aac2ea77996bdf56ced37ad9d0
Jim Ladd <jladd@tuta.io>
2025-12-26 19:35:24 -0800

    fix: use incremental numbers not file hashes for block filenames

--- pad/tests/test_pad.py
+++ pad/tests/test_pad.py
@@ -10,54 +10,6 @@ from perfect_pad import pad
 from perfect_pad.settings import MAX_BLOCK_SIZE, STD_BLOCK_SIZE
 
 
-class TestCreateOneTimePad:
-    def test_create_one_time_pad_fails_if_path_parent_dir_does_not_exist(self):
-        path_missing_parent_folder = Path("/not-a-real-folder/my.pad")
-        with pytest.raises(ValueError) as excinfo:
-            pad.create_one_time_pad(path_missing_parent_folder, 100)
-        expected_description = (
-            "Cannot create one-time pad at "
-            + "/not-a-real-folder/my.pad; "
-            + "parent directory does not exist"
-        )
-        assert excinfo.value.args[0] == expected_description
-
-    def test_create_one_time_pad_fails_if_path_already_exists(self):
-        with TemporaryDirectory() as tmp_dir:
-            with pytest.raises(ValueError) as excinfo:
-                pad.create_one_time_pad(Path(tmp_dir), 100)
-            expected_description = (
-                "Cannot create one-time pad at "
-                + f"{tmp_dir}; file or directory "
-                + "already present at location"
-            )
-            assert excinfo.value.args[0] == expected_description
-
-        with NamedTemporaryFile() as tmp_file:
-            with pytest.raises(ValueError) as excinfo:
-                pad.create_one_time_pad(Path(tmp_file.name), 100)
-            expected_description = (
-                "Cannot create one-time pad at "
-                + f"{tmp_file.name}; file or directory "
-                + "already present at location"
-            )
-            assert excinfo.value.args[0] == expected_description
-
-    @mock.patch("perfect_pad.pad.create_block_file")
-    def test_create_one_time_pad_creates_correct_number_of_blocks(
-        self, mock_create_block_file
-    ):
-        pad_size = 10**6
-        with TemporaryDirectory() as tmp_dir:
-            chmod(tmp_dir, 0o700)
-            new_dir = Path(tmp_dir).joinpath("foo.pad")
-            pad.create_one_time_pad(new_dir, pad_size)
-            calls = mock_create_block_file.mock_calls
-            assert len(calls) == ceil(pad_size / STD_BLOCK_SIZE)
-            for call in calls:
-                assert call.args == (new_dir, STD_BLOCK_SIZE)
-
-
 class TestCreateBlockFile:
     def test_create_block_file_refuses_to_create_huge_block(self):
         """Ensure that `create_block_file` requires the `size`
@@ -77,30 +29,22 @@ class TestCreateBlockFile:
 
     @mock.patch("perfect_pad.pad.os")
     @mock.patch("perfect_pad.pad.open")
-    @mock.patch("perfect_pad.pad.sha3_256")
     @mock.patch("perfect_pad.pad.get_random_bytes")
     def test_create_block_file_creates_block_file_at_path(
-        self, mock_get_random_bytes, mock_sha3_256, mock_open, mock_os
+        self, mock_get_random_bytes, mock_open, mock_os
     ):
         """Ensure that `create_block_file` creates a block file at `path`"""
-        fake_sha = "fabbafcab"
-        fake_full_path = f"/tmp/pads/foo.pad/{fake_sha}"
-        fake_full_path_obj = Path(fake_full_path)
+        fake_path = f"/tmp/pads/foo.pad/0000000000000000"
+        fake_path_obj = Path(fake_path)
 
-        mock_path = mock.MagicMock()
-        mock_path.joinpath.return_value = fake_full_path_obj
-
-        mock_sha3_256.return_value.hexdigest.return_value = fake_sha
-
-        pad.create_block_file(mock_path, 100)
-
-        mock_path.joinpath.assert_called_once_with(fake_sha)
+        res = pad.create_block_file(fake_path_obj, 100)
+        assert res is None
 
         mock_open_calls = mock_open.return_value.__enter__.return_value.mock_calls
         assert len(mock_open_calls) == 1
-        mock_open_calls[0].args[0].as_posix() == fake_full_path
+        mock_open_calls[0].args[0].as_posix() == fake_path
 
-        mock_os.chmod.assert_called_once_with(fake_full_path_obj, 0o400)
+        mock_os.chmod.assert_called_once_with(fake_path_obj, 0o400)
 
 
 class TestFetchBlocks:
@@ -150,3 +94,52 @@ class TestFetchBlocks:
 
         mock_get_random_block.assert_called_once_with(pad_path)
         mock_fetch_and_destroy_block.assert_called_once_with(pad_path, "foo")
+
+
+class TestCreateOneTimePad:
+    def test_create_one_time_pad_fails_if_path_parent_dir_does_not_exist(self):
+        path_missing_parent_folder = Path("/not-a-real-folder/my.pad")
+        with pytest.raises(ValueError) as excinfo:
+            pad.create_one_time_pad(path_missing_parent_folder, 100)
+        expected_description = (
+            "Cannot create one-time pad at "
+            + "/not-a-real-folder/my.pad; "
+            + "parent directory does not exist"
+        )
+        assert excinfo.value.args[0] == expected_description
+
+    def test_create_one_time_pad_fails_if_path_already_exists(self):
+        with TemporaryDirectory() as tmp_dir:
+            with pytest.raises(ValueError) as excinfo:
+                pad.create_one_time_pad(Path(tmp_dir), 100)
+            expected_description = (
+                "Cannot create one-time pad at "
+                + f"{tmp_dir}; file or directory "
+                + "already present at location"
+            )
+            assert excinfo.value.args[0] == expected_description
+
+        with NamedTemporaryFile() as tmp_file:
+            with pytest.raises(ValueError) as excinfo:
+                pad.create_one_time_pad(Path(tmp_file.name), 100)
+            expected_description = (
+                "Cannot create one-time pad at "
+                + f"{tmp_file.name}; file or directory "
+                + "already present at location"
+            )
+            assert excinfo.value.args[0] == expected_description
+
+    @mock.patch("perfect_pad.pad.create_block_file")
+    def test_create_one_time_pad_creates_correct_number_of_blocks(
+        self, mock_create_block_file
+    ):
+        pad_size = 10**6
+        with TemporaryDirectory() as tmp_dir:
+            chmod(tmp_dir, 0o700)
+            new_dir = Path(tmp_dir).joinpath("foo.pad")
+            pad.create_one_time_pad(new_dir, pad_size)
+            calls = mock_create_block_file.mock_calls
+            assert len(calls) == ceil(pad_size / STD_BLOCK_SIZE)
+            for i, call in enumerate(calls):
+                block_name = str(i).zfill(16)
+                assert call.args == (new_dir.joinpath(block_name), STD_BLOCK_SIZE)
